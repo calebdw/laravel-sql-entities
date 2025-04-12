c76c30e4cf1131956cd63316e935e88e8e67fd4b
Caleb White <cdwhite3@pm.me>
2025-04-12 18:23:12 -0500

    feat: add phpstan support for SqlEntity facade

--- src/Facades/SqlEntity.php
+++ src/Facades/SqlEntity.php
@@ -4,11 +4,20 @@ declare(strict_types=1);
 
 namespace CalebDW\SqlEntities\Facades;
 
+use CalebDW\SqlEntities\SqlEntity as SqlEntityBase;
 use CalebDW\SqlEntities\SqlEntityManager;
 use Illuminate\Support\Facades\Facade;
 use Override;
 
-/** @mixin SqlEntityManager */
+/**
+ * @method static SqlEntityBase get(string $name)
+ * @method static void create(SqlEntityBase|class-string<SqlEntityBase>|string $entity)
+ * @method static void drop(SqlEntityBase|class-string<SqlEntityBase>|string $entity)
+ * @method static void createAll(?string $type = null, ?string $connection = null)
+ * @method static void dropAll(?string $type = null, ?string $connection = null)
+ *
+ * @see SqlEntityManager
+ */
 class SqlEntity extends Facade
 {
     #[Override]
